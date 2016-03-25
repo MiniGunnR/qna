987f9fa7daca29c89c2d6c7733d15a41a4304607
Mohaiminul Hasan <hasan.mohaiminul@gmail.com>
2016-03-25 19:05:46 +0600

    action views now work

--- qna/alpha/urls.py
+++ qna/alpha/urls.py
@@ -1,5 +1,17 @@
 from django.conf.urls import url
 
-urlpatterns = [
+from . import views
 
+urlpatterns = [
+    url(r'^heart/question/(?P<pk>\d+)/', views.QuestionHeartView, name='question-heart'),
+    url(r'^heart/answer/(?P<pk>\d+)/', views.AnswerHeartView, name='answer-heart'),
+
+    url(r'^flag/question/(?P<pk>\d+)/', views.QuestionFlagView, name='question-flag'),
+    url(r'^flag/answer/(?P<pk>\d+)/', views.AnswerFlagView, name='answer-flag'),
+
+    url(r'^heart/question/comment/(?P<pk>\d+)/', views.QuestionCommentHeartView, name='question-comment-heart'),
+    url(r'^heart/answer/comment/(?P<pk>\d+)/', views.AnswerCommentHeartView, name='answer-comment-heart'),
+
+    url(r'^flag/question/comment/(?P<pk>\d+)/', views.QuestionCommentFlagView, name='question-comment-flag'),
+    url(r'^flag/answer/comment/(?P<pk>\d+)/', views.AnswerCommentFlagView, name='answer-comment-flag'),
 ]
--- qna/alpha/views.py
+++ qna/alpha/views.py
@@ -1,3 +1,77 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from .models.models import Question, QuestionFlag, QuestionHeart, QuestionCommentHeart, QuestionCommentFlag,\
+    Answer, AnswerFlag, AnswerHeart, AnswerCommentHeart, AnswerCommentFlag
+
+def QuestionHeartView(request, pk):
+    obj, created = QuestionHeart.objects.get_or_create(question_id=pk, user=request.user)
+
+    if not created:
+        obj.delete()
+        return JsonResponse({"response": "unhearted"})
+    else:
+        return JsonResponse({"response": "hearted"})
+
+def AnswerHeartView(request, pk):
+    obj, created = AnswerHeart.objects.get_or_create(answer_id=pk, user=request.user)
+
+    if not created:
+        obj.delete()
+        return JsonResponse({"response": "unhearted"})
+    else:
+        return JsonResponse({"response": "hearted"})
+
+def QuestionFlagView(request, pk):
+    obj, created = QuestionFlag.objects.get_or_create(question_id=pk, user=request.user)
+
+    if not created:
+        obj.delete()
+        return JsonResponse({"response": "unflagged"})
+    else:
+        return JsonResponse({"response": "flagged"})
+
+def AnswerFlagView(request, pk):
+    obj, created = AnswerFlag.objects.get_or_create(answer_id=pk, user=request.user)
+
+    if not created:
+        obj.delete()
+        return JsonResponse({"response": "unflagged"})
+    else:
+        return JsonResponse({"response": "flagged"})
+
+def QuestionCommentHeartView(request, pk):
+    obj, created = QuestionCommentHeart.objects.get_or_create(comment_id=pk, user=request.user)
+
+    if not created:
+        obj.delete()
+        return JsonResponse({"response": "unhearted"})
+    else:
+        return JsonResponse({"response": "hearted"})
+
+def AnswerCommentHeartView(request, pk):
+    obj, created = AnswerCommentHeart.objects.get_or_create(comment_id=pk, user=request.user)
+
+    if not created:
+        obj.delete()
+        return JsonResponse({"response": "unhearted"})
+    else:
+        return JsonResponse({"response": "hearted"})
+
+def QuestionCommentFlagView(request, pk):
+    obj, created = QuestionCommentFlag.objects.get_or_create(comment_id=pk, user=request.user)
+
+    if not created:
+        obj.delete()
+        return JsonResponse({"response": "unflagged"})
+    else:
+        return JsonResponse({"response": "flagged"})
+
+def AnswerCommentFlagView(request, pk):
+    obj, created = AnswerCommentFlag.objects.get_or_create(comment_id=pk, user=request.user)
+
+    if not created:
+        obj.delete()
+        return JsonResponse({"response": "unflagged"})
+    else:
+        return JsonResponse({"response": "flagged"})
